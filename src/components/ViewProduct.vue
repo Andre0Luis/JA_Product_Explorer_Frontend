50bd4f6f92dac9fed4373c7bb5389721e92c5b0e
Andre0Luis <aluis283@gmail.com>
2024-08-22 17:39:35 -0300

    integration with backend

--- src/components/ViewProduct.vue
+++ src/components/ViewProduct.vue
@@ -1,39 +1,38 @@
+<script setup lang="ts">
+import { ref, defineProps } from 'vue';
+import { Product } from '@/model/ProductDTO';
+
+const props = defineProps<{product: Product | null}>();
+
+</script>
+
 <template>
-    <div>
+    <div v-if="props.product && Object.keys(props.product).length > 0">
         <div class="product-info">
             <h2>Produto</h2>
-            <p>{{ product }}</p>
+            <p>{{ product?.nome }}</p>
             <hr>
             <h2>Preço</h2>
-            <p>{{ price }}</p>
+            <p>{{ product?.preco }}</p>
             <hr>
             <h2>Estoque</h2>
-            <p>{{ stock }}</p>
+            <p>{{ product?.estoque }}</p>
+            <hr>
+            <h2>Última Atualização</h2>
+            <p>{{ product?.dataAtualizacao }}</p>
+            <hr>
+            <h2>Código de Barras</h2>
+            <p>{{ product?.codigoDeBarras }}</p>
         </div>
+    </div>
+    <div class="error" v-else>
+      <p>Produto não encontrado</p>
+      <transition name="blink">
+        <div v-if="props.product && Object.keys(props.product).length === 0" key="blink"></div>
+      </transition>
     </div>
 </template>
 
-<script setup lang="ts">
-// Your component's TypeScript logic goes here
-import { ref } from 'vue';
-
-// Define reactive variables
-const product = ref('');
-const price = ref(0);
-const stock = ref(0);
-
-// Function to fetch product data
-const fetchProductData = () => {
-    // Make an API call or fetch data from a source
-    // Assign the received values to the reactive variables
-    product.value = 'Luizinho';
-    price.value = 1.99;
-    stock.value = 1;
-};
-
-// Call the fetchProductData function to populate the data initially
-fetchProductData();
-</script>
 
 <style scoped>
 /* Your component's CSS styles go here */
@@ -57,6 +56,27 @@ fetchProductData();
 .product-info p {
     font-size: 2.0rem;
     margin-block: 10px;
+}
+.error p {
+    background-color: #f2f2f2;
+    color: #333;
+    padding: 10px;
+    border-radius: 5px;
+    font-weight: bold;
+    font-weight: bold;
+    margin-block: 10px;
+    text-align: center;
+}
+
+.blink-enter-active, .blink-leave-active {
+  animation: blink 3s infinite;
+}
+.blink-enter, .blink-leave-to /* .blink-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+@keyframes blink {
+  0%, 100% { opacity: 1; }
+  50% { opacity: 0; }
 }
 
 </style>
