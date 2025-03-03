fd100f3369a3f0c1610bd5be3231088ce55ce48b
Andre0Luis <aluis283@gmail.com>
2025-03-03 14:35:05 -0300

    chore: connection ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ConsultView from '@/views/ConsultView.vue'
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router';
+
+const HomeView = () => import('../views/HomeView.vue');
+const AboutView = () => import('../views/AboutView.vue');
+const ConsultView = () => import('../views/ConsultView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/consulta',
-      name: 'consulta',
-      component: ConsultView
-    }
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/about', name: 'about', component: AboutView },
+    { path: '/consulta', name: 'consulta', component: ConsultView }
   ]
-})
+});
 
-export default router
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 2048,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
     }
   }
-})
+});
